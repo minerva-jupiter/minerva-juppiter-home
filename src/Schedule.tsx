71c19fbb5e9aee4dc8568dc03420c25d41f1b0c3
Minerva_juppiter <ryouturn@gmail.com>
2024-11-06 21:55:20 +0900

    add google calendar

--- src/Schedule.tsx
+++ src/Schedule.tsx
@@ -5,7 +5,7 @@ const Schedule = () => {
     return(
         <Container className='text-center'>
             <h1 style={{color:"white"}}>Schedule</h1>
-            <p>here will be calendar</p>
+            <iframe src="https://calendar.google.com/calendar/embed?height=800&wkst=1&ctz=Asia%2FTokyo&src=cnlvdXR1cm5AZ21haWwuY29t&color=%23039BE5" style={{border:"solid 1px #777"}} width="1000" height="800" />
         </Container>
     )
 }
