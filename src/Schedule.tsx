f30089d9855ff9afa103e830aa0058a9d1b8e3dd
Minerva_juppiter <ryouturn@gmail.com>
2024-11-07 07:06:02 +0900

    fixed calendar

--- src/Schedule.tsx
+++ src/Schedule.tsx
@@ -2,10 +2,11 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { Container } from 'react-bootstrap';
 
 const Schedule = () => {
+    let innerWidth = window.innerWidth / 1.5;
     return(
         <Container className='text-center'>
             <h1 style={{color:"white"}}>Schedule</h1>
-            <iframe src="https://calendar.google.com/calendar/embed?height=800&wkst=1&ctz=Asia%2FTokyo&src=cnlvdXR1cm5AZ21haWwuY29t&color=%23039BE5" style={{border:"solid 1px #777"}} width="1000" height="800" />
+            <iframe src="https://calendar.google.com/calendar/embed?wkst=1&ctz=Asia%2FTokyo&src=cnlvdXR1cm5AZ21haWwuY29t&color=%23039BE5" style={{border:"solid 1px #777"}} width={innerWidth} height={innerWidth/4*3} />
         </Container>
     )
 }
