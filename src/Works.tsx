4f36415801c3f78ae4418246be7ca0e553d14148
minerva-jupitr <ryouturn@gmail.com>
2024-11-18 19:02:53 +0900

    update and start dev linkcard

--- src/Works.tsx
+++ src/Works.tsx
@@ -1,11 +1,13 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Container } from 'react-bootstrap';
 import UnderConstruction from './UnderConstruction';
+import LinkCard from './LinkCard';
 
 const Works = () => {
     return(
         <Container className='text-center'>
             <h1 style={{color:"white"}}>Works</h1>
+            <LinkCard/>
             <UnderConstruction></UnderConstruction>
         </Container>
     )
