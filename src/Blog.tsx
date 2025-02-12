4ab8727c59784526900fde0b4bb31dc8ad8c5900
minerva-jupitr <ryouturn@gmail.com>
2025-02-12 16:46:18 +0900

    add br to blog

--- src/Blog.tsx
+++ src/Blog.tsx
@@ -4,7 +4,18 @@ import { Card, Container } from 'react-bootstrap';
 const Blog = () => {
     return(
         <Container className='text-center'>
+            <br />
             <h1 style={{color:'white'}}>Blog</h1>
+            <br />
+            <Card>
+                <Card.Title>
+                    カバンを自作する記事を書きました
+                </Card.Title>
+                <Card.Body>
+                    島根大学ものづくり部が発行する、2024年度の部誌に「PCカバン設計のすゝめ」という記事を寄稿させていただきました。
+                </Card.Body>
+            </Card>
+            <br />
             <Card>
                 <Card.Title>
                     自分のウェブページを作ってみた
